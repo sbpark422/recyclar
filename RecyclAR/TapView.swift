7e655491d0423f38f71bed9e36c3680bb5111a15
Park, Soo Bin <sbpark422@naver.com>
2023-04-11 19:47:32 -0400

    Update TapView

---
+++ RecyclAR/TapView.swift
@@ -0,0 +1,20 @@
+//
+//  TapView.swift
+//  RecyclAR
+//
+//  Created by Soo Bin on 4/11/23.
+//
+
+import SwiftUI
+
+struct TapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TapView_Previews: PreviewProvider {
+    static var previews: some View {
+        TapView()
+    }
+}
